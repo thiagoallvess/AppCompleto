ab5c1bcf98f664e18ac4c4fe0c02cd3e04731504
[dyad] <thiago.allvess@gmail.com>
2025-12-20 20:38:29 -0300

    [dyad] Updating product grid to display 4 products per line - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -97,7 +97,7 @@ const Index = () => {
 
       {/* Products Grid */}
       <main className="flex-1 px-4 pb-24">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="grid grid-cols-4 gap-4">
           {filteredProducts.map((product) => (
             <Link
               key={product.id}
