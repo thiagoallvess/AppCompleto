33bd2910652db09c442a588950efdea21d5a3d23
[dyad] <thiago.allvess@gmail.com>
2025-12-22 10:49:10 -0300

    [dyad] Fixed incorrect Dialog import in GestaoEstoque.tsx - wrote 1 file(s)

--- src/pages/GestaoEstoque.tsx
+++ src/pages/GestaoEstoque.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dropdown-menu";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog";
 import { showSuccess, showError } from "@/utils/toast";
 
 const GestaoEstoque = () => {
